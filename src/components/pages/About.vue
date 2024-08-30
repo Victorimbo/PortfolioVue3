5b02b7ace3457ab296fc64f380f5438aba03ad24
Victor <victor.imbo@epitech.eu>
2024-08-30 10:49:08 +0200

    update CV

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -3,7 +3,7 @@
     <p>Bored by studies in high school, I started to work at 18 in hospitality. I was a bartender for 7 years in France and United kingdom.<br>
     Some times ago I realised I wanted to go back to studies in something that intereted me : Programming. I started studying at Epitech in november 2022.<br>
     I have never code before but it feels great overall ! You want to know more about me ? 
-    <a href="/assets/Victor-imbo.pdf" download="Victor-Imbo.pdf">Download my awesome CV !</a>
+    <a href="/assets/Victor-imboEpitech.pdf" download="Victor-Imbo.pdf">Download my awesome CV !</a>
     <p>My main competencies are :</p>
     <p>Conceptualize a web solution (write a specification, deploy a work environnment, Write a presentation, ...)</p>
     <p>Develop a web solution (write the solution code, implement front-end and database, deploy the solution, ...)</p>
