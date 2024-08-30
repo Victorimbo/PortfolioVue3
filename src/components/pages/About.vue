582492a72e0fe53407ead319a3dd3ba1bd150748
Victor <victor.imbo@epitech.eu>
2024-08-30 10:37:59 +0200

    add competencies

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -4,7 +4,11 @@
     Some times ago I realised I wanted to go back to studies in something that intereted me : Programming. I started studying at Epitech in november 2022.<br>
     I have never code before but it feels great overall ! You want to know more about me ? 
     <a href="/assets/Victor-imbo.pdf" download="Victor-Imbo.pdf">Download my awesome CV !</a>
-  </p>
+    <p>My main competencies are :</p>
+    <p>Conceptualize a web solution (write a specification, deploy a work environnment, Write a presentation, ...)</p>
+    <p>Develop a web solution (write the solution code, implement front-end and database, deploy the solution, ...)</p>
+    <p>Deploy a quality assurance system throughout the life cycle of a web solution (write documentation, improve the solution, fix bugs, ...)</p>
+    </p>
 
 <TypeWriter />
 </div>
